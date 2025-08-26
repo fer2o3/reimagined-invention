eab7f8fdb69e0c6a83e68905821ff7818454f720
Benjamin Andre <benjaminandre23@gmail.com>
2025-08-26 06:12:33 +0200

    feat(005): completed

--- src/005.py
+++ src/005.py
@@ -1,3 +1,15 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
 def solve():
-    # TODO:
-    return None
+    res = 1
+    for n in range(1, 21):
+        res = lcm(res, n)
+    return res
