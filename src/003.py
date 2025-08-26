e3b11fe8bbc5ced59edde732e28750287a12a69e
Benjamin Andre <benjaminandre23@gmail.com>
2025-08-26 18:30:59 +0200

    feat(003): completed

--- src/003.py
+++ src/003.py
@@ -1,3 +1,9 @@
 def solve():
-    # TODO:
-    return None
+    n = 600851475143
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
