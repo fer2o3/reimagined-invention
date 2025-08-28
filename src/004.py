1af16006c0bfc7617dda0c089bb9dc30ee8fd757
Benjamin Andre <benjaminandre23@gmail.com>
2025-08-28 07:31:41 +0200

    feat(004): completed

--- src/004.py
+++ src/004.py
@@ -1,3 +1,8 @@
 def solve():
-    # TODO:
-    return None
+    palindromes = []
+    for i in range(100, 1000):
+        for j in range(i, 1000):
+            prod = i * j
+            if str(prod) == str(prod)[::-1]:
+                palindromes.append(prod)
+    return max(palindromes)
