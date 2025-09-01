4dbe81238d47311a77c7fa4b9bf20e0f476a8ab1
Benjamin Andre <benjaminandre23@gmail.com>
2025-09-01 21:30:14 +0200

    feat(009): completed

--- src/009.py
+++ src/009.py
@@ -1,3 +1,7 @@
 def solve():
-    # TODO:
-    return None
+    for a in range(1, 1000):
+        for b in range(a, 1000):
+            for c in range(b, 1000):
+                if a + b + c == 1000 and a * a + b * b == c * c:
+                    print(a, b, c)
+                    return a * b * c
