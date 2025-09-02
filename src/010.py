4c857cc8b6fcc247ca19ba777c7504e95fcc6b23
Benjamin Andre <benjaminandre23@gmail.com>
2025-09-02 19:34:25 +0200

    feat(010): completed

--- src/010.py
+++ src/010.py
@@ -1,3 +1,16 @@
 def solve():
-    # TODO:
-    return None
+    num = 2000000
+    prime = [True for _ in range(num + 1)]
+    p = 2
+    while p * p <= num:
+        if prime[p]:
+            for i in range(p * p, num + 1, p):
+                prime[i] = False
+        p += 1
+
+    sum = 0
+    for p in range(2, num + 1):
+        if prime[p]:
+            sum += p
+
+    return sum
