67fe5f0dfbba4a6eaead397a25c404ddf5fc8f00
Benjamin Andre <benjaminandre23@gmail.com>
2025-09-07 06:36:48 +0200

    feat(014): completed

--- src/014.py
+++ src/014.py
@@ -1,3 +1,23 @@
+def chain(n, memo):
+    if n in memo:
+        return memo[n]
+
+    if n % 2 == 0:
+        nxt = n // 2
+    else:
+        nxt = 3 * n + 1
+
+    memo[n] = 1 + chain(nxt, memo)
+    return memo[n]
+
+
 def solve():
-    # TODO:
-    return None
+    memo = {1: 1}
+    max_i, max_len = 1, 1
+
+    for i in range(2, 1000001):
+        length = chain(i, memo)
+        if length > max_len:
+            max_len, max_i = length, i
+
+    return max_i
