3e6f75b41c055c86f5e7b9b40432defa3fe32450
Benjamin Andre <benjaminandre23@gmail.com>
2025-09-08 06:19:59 +0200

    feat(012): completed

--- src/012.py
+++ src/012.py
@@ -1,3 +1,20 @@
+def count_divisors(n):
+    i = 2
+    res = 0
+    while i * i <= n:
+        if i * i == n:
+            res += 1
+        elif n % i == 0:
+            res += 2
+        i += 1
+    return res
+
+
 def solve():
-    # TODO:
-    return None
+    res = 0
+    triangle = 0
+    for i in range(9999999):
+        triangle += i
+        if count_divisors(triangle) > 500:
+            break
+    return triangle
