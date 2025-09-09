d9cd9008eff3d1464190911e5e5cae9ced4bc434
Benjamin Andre <benjaminandre23@gmail.com>
2025-09-09 06:13:21 +0200

    feat(015): completed

--- src/015.py
+++ src/015.py
@@ -1,3 +1,9 @@
+def factorial(n):
+    res = 1
+    for i in range(2, n + 1):
+        res *= i
+    return res
+
+
 def solve():
-    # TODO:
-    return None
+    return factorial(2 * 20) // (factorial(20) * factorial(20))
